e225e28c7eee677460f230e13d93c12e514ecba4
Vivek Agarwal <nitkkr.vivek@gmail.com>
2017-02-03 23:01:20 -0800

    fix

---
+++ cc/KOL1502.cc
@@ -0,0 +1,16 @@
+#include <cstdio>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int T;
+  scanf("%d", &T);
+  while(T--) {
+    int N; scanf("%d", &N);
+    int A[N];
+    for (int i = 0 ; i < N ; i++) scanf("%d", &A[i]);
+    long long numDishes[N][20 * N + 1];
+    for (int i = 0 ; i < 
+  }
+}
